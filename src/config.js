c6df66e94b11e22aca68a006233198b079030f77
AilaVarun <156280955+AilaVarun@users.noreply.github.com>
2025-05-07 03:26:24 +0000

    UrL update

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // TODO: Replace this with your own Server URL for your codespace
-export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://friendly-space-garbanzo-q775xx5qgxqvhxx9q-27017.app.github.dev/";
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-
-// src/config.js
-
-export const BASE_URL = "https://friendly-space-garbanzo-q775xx5qgxqvhxx9q-3080.app.github.dev";
 export default defineConfig({
   plugins: [react()],
   test: {
@@ -13,7 +9,3 @@ export default defineConfig({
     environment: 'jsdom',
   },
 })
-
-
-
-
